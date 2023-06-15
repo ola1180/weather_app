ae5532ce935cd533a3d5185aef00058f3669ca74
Aleksandra Kurdel <aleksandra.kurdel@gmail.com>
2023-06-15 21:02:32 +0200

    added API

---
+++ assets/js/app.js
@@ -0,0 +1,54 @@
+let weatherForm = document.querySelector('.weather__form')
+let cityInput = document.querySelector('.weather__city')
+let apiUrl = 'http://api.weatherapi.com/v1/current.json?key=a2d9f72be70a4af298b165515231506&aqi=no&q='
+let apiDataContainer = document.querySelector('.weather__data')
+
+weatherForm.addEventListener('submit', (event) => {
+
+    let city = cityInput.value
+    let fullApiURl = apiUrl + city
+
+    fetch(fullApiURl)
+    .then(response => {
+        if(response.status === 200) {
+            return response.json()
+        }
+
+        throw new Error()
+    })
+
+    .then((dataFromApi)=> {
+        // console.log(dataFromApi.current.temp_c)
+        let view = ``
+        view += `<div class="weather__info">`
+        // county, city, time(region)
+        view += `<div class="weather__region"> 
+                <p>${dataFromApi.location.name}</p>
+                <p>${dataFromApi.location.country}</p>
+                <p> ${dataFromApi.location.localtime}</p>
+        </div>`
+ 
+        //icon
+        view += `<div class="weather__icon"><img src= ${dataFromApi.current.condition.icon} 
+        alt = ${dataFromApi.current.condition.text}></div >`
+        //temp
+        view += `<div class="weather__temp"> ${dataFromApi.current.temp_c} <span>&degC</span></div >`
+ 
+        //details
+        view += `<div class="weather__details">
+                <p>The amount of rainfall: ${dataFromApi.current.precip_mm}mm </p>
+                <p>Humidity: ${dataFromApi.current.humidity}%</p>
+                <p>Wind: ${dataFromApi.current.wind_kph}km/h</p>
+            </div>`
+ 
+        view += `</div>`
+
+        apiDataContainer.innerHTML = view
+    }).catch((error) => showError())
+
+    event.preventDefault()
+})
+
+let showError = () => {
+    apiDataContainer.innerHTML = `<div class='weather__error'>City not found or thera are server problem</div>`
+}
