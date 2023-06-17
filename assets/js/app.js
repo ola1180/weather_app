dbb5137c084334f768ea4d97217f9bd15b24fd70
Aleksandra Kurdel <aleksandra.kurdel@gmail.com>
2023-06-17 11:51:16 +0200

    added pwa

--- assets/js/app.js
+++ assets/js/app.js
@@ -77,3 +77,12 @@ let searchPosition = (position) => {
     cityInput.value = `${position.coords.latitude},${position.coords.longitude}`
     weatherForm.requestSubmit()
 }
+
+if ("serviceWorker" in navigator) {
+	window.addEventListener("load", function () {
+    	navigator.serviceWorker
+        	.register("serviceWorker.js")
+        	.then(res => console.log("service worker registered"))
+        	.catch(err => console.log("service worker not registered", err))
+	})
+}
